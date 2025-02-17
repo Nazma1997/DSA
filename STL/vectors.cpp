6b65282267f85c8e0e6290a871520c5b1b3c55a3
nazma1997 <nazma.ist.707@gmail.com>
2025-02-17 23:53:56 +0600

    add and remove into last

--- STL/vectors.cpp
+++ STL/vectors.cpp
@@ -1,36 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    vector<string> cars = {"Volvo", "BMW", "Ford", "Mazda"};
-    int i;
-    for(i=0; i < cars.size(); i++){
-   //   cout<< cars[i] << endl;
-    }
+int main()
+{
+  vector<string> cars = {"Volvo", "BMW", "Ford", "Mazda"};
+  int i;
+  for (i = 0; i < cars.size(); i++)
+  {
+    //   cout<< cars[i] << endl;
+  }
 
-    // Access a element fromn  vector
+  // Access a element fromn  vector
   //  cout<< cars[0] << endl;
 
-    // Access 1st element
-   // cout << cars.front() << endl;
+  //  Access 1st element
+  // cout << cars.front() << endl;
 
-    // Access last element
-   // cout << cars.back() << endl;
+  // Access last element
+  // cout << cars.back() << endl;
 
-   // To access a specific element
-   //cout << cars.at(0);
+  // To access a specific element
+  // cout << cars.at(0);
 
   // cout << cars.at(cars.size()-2) << endl;
 
-  //insert an item in the vector
+  // insert an item in the vector
 
-  cars.push_back("Corolla");
-  cars.push_back("Jira");
+  /* cars.push_back("Corolla");
+   cars.push_back("Jira");
+   for (i = 0 ; i< cars.size(); i++){
+     cout << cars[i] << endl;
+   }
+ */
+
+  /*
+    Remove last item from a vector
+
+
+  cars.pop_back();
+
+
   for (i = 0 ; i< cars.size(); i++){
-    cout << cars[i] << endl;
-  }
+      cout << cars[i] << endl;
+    }
+    */
 
-
-
-   
+  return 0;
 }
